bdfd62218b99227031ea01fa1de3295d6f198b89
Athanasios-Ilias <athanrous@gmail.com>
2011-12-10 20:33:04 +0200

    Create mycraaawler.py file

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+import urllib
+import urllib2
+import argparse
+
+from BeautifulSoup import BeautifulSoup as bs
+
+parser = argparse.ArgumentParser(description = "Let's craawl the web guys!")
+
+parser.add_argument('url', nargs = 1, help = 'target url')
+
+parser.add_argument('-n' , '--number-of-levels', type = int , default = 1 , help = 'how deep')
+
+args = parser.parse_args()
+
+target_url = args.url.pop()
+
+deep = args.number_of_levels
+
+page = urllib2.urlopen(target_url)
+
+user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
+
+_opener = urllib2.build_opener ()
+
+_opener.addheaders = [('User - agent' , user_agent ) ]
+
+raw_code =_opener.open(target_url).read() # Toda la informacion de la pagina esta aqui
+
+soup = bs(raw_code)
+
+for link in soup.findAll('a') :
+    if link.has_key('href') :
+        a = link.get('href')
+        print a
+        
