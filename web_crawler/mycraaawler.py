1349dafa2761170306ce339adc94e6f8b01b94a3
Athanasios-Ilias <athanrous@gmail.com>
2011-12-14 01:54:55 +0200

    Change the name "target_url" into "enlance" intro the Enlances function

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -6,7 +6,19 @@ import argparse
 
 from BeautifulSoup import BeautifulSoup as bs
 
+def Enlances (nivel,asterisko,enlance):
+    raw_code =_opener.open(enlance).read()
+    soup = bs(raw_code)
+    todos_enlances = [link ('href')
+                  for link in soup.findAll('a')
+                  if link.has_key('href')] # Aqui hago el "for" y el "if" en la misma manera con las trasparencias
 
+    for a in todos_enlances : 
+            print asterisko , a
+            if nivel == 1 :
+                    print "Something went wrong,very low level,please retry"
+            if nivel < deep :    
+                    Enlances(nivel+1,asterisko+'*',a) 
 
 parser = argparse.ArgumentParser(description = "Let's craawl the web guys!")
 
@@ -20,24 +32,12 @@ target_url = args.url.pop()
 
 deep = args.number_of_levels
 
-page = urllib2.urlopen(target_url)
+#page = urllib2.urlopen(target_url)
 
 global asterisko # Pongo global la variable porque se utilizara dentro la function
 asterisko = ' * '
 
-def Enlances (nivel,asterisko,target_url):
-    raw_code =_opener.open(target_url).read()
-    soup = bs(raw_code)
-    todos_enlances = [link ('href')
-                  for link in soup.findAll('a')
-                  if link.has_key('href')] # Aqui hago el "for" y el "if" en la misma manera con las trasparencias
-
-    for a in todos_enlances : 
-            print asterisko , a
-            if nivel == 1 :
-                    print "Something went wrong,very low level,please retry"
-            if nivel < deep :    
-                    Enlances(nivel+1,asterisko+'*',a)      
+     
 
 user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
 
