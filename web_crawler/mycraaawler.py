e7c78e6e86226f57ee8723975e517b656e3b72c7
Athanasios-Ilias <athanrous@gmail.com>
2011-12-14 00:02:25 +0200

    Change the sequence of the source code.

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -6,6 +6,8 @@ import argparse
 
 from BeautifulSoup import BeautifulSoup as bs
 
+
+
 parser = argparse.ArgumentParser(description = "Let's craawl the web guys!")
 
 parser.add_argument('url', nargs = 1, help = 'target url')
@@ -19,14 +21,6 @@ target_url = args.url.pop()
 deep = args.number_of_levels
 
 page = urllib2.urlopen(target_url)
-
-user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
-
-_opener = urllib2.build_opener ()
-
-_opener.addheaders = [('User - agent' , user_agent ) ]
-
-raw_code =_opener.open(target_url).read() # Toda la informacion de la pagina esta aqui
 
 global asterisko # Pongo global la variable porque se utilizara dentro la function
 asterisko = ' * '
@@ -43,3 +37,14 @@ def Enlances (nivel,asterisko,target_url):
                     print "Something went wrong,very low level,please retry"
             if nivel < deep :    
                     Enlances(nivel+1,asterisko+'*',a)      
+
+user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
+
+_opener = urllib2.build_opener ()
+
+_opener.addheaders = [('User - agent' , user_agent ) ]
+
+raw_code =_opener.open(target_url).read() # Toda la informacion de la pagina esta aqui
+
+
+
