b94777194bda1fff40f524ea5efe15be5891be8e
Athanasios-Ilias <athanrous@gmail.com>
2011-12-14 14:06:51 +0200

    Recreate the file mycraaawler.py due to force delete

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -8,28 +8,23 @@ import argparse
 from BeautifulSoup import BeautifulSoup as bs
 
 
-def obtainLink(level,asterisko,url):
+def Enlances (nivel,asterisko,url):
     try:
-        user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
-
-        _opener = urllib2.build_opener()
-        _opener.addheaders = [('User - agent',user_agent ) ]
         raw_code =_opener.open(url).read()
         soup = bs( raw_code )
         todos_enlances = [ link ('href') for link
             in soup.findAll('a')
-            if link.has_key('href')] 
+            if link.has_key('href')]
     except urllib2.URLError :
-        if level == 1:
+        if nivel == 1:
             print "Something went wrong,very low level,please retry"
-        return   
+        return
     
-    for link in todos_enlances : 
-        #print link  
-        if level < deep :   
-                obtainLink(level+1,asterisko+'*',link)  
-                print "Hello World!"
-          
+    for link in todos_enlances :
+        print link
+        if nivel < deep :
+            Enlances(nivel+1,asterisko+'*',link)
+           
 parser = argparse.ArgumentParser(description = "Let's craawl the web guys!")
 
 parser.add_argument('url', nargs = 1, help = 'target url')
@@ -42,9 +37,11 @@ target_url = args.url.pop()
 
 deep = args.number_of_levels
 
-obtainLink(1,"*",target_url) 
+user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
 
-    
+_opener = urllib2.build_opener()
+        
+_opener.addheaders = [('User - agent',user_agent ) ]
+        
 
-
-
+Enlances(1,"*",target_url) 
