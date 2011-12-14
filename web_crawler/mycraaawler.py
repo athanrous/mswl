a1d885d83a7d6623b44c3996040a07fea1c00761
Athanasios-Ilias <athanrous@gmail.com>
2011-12-14 12:44:37 +0200

    Change Function and variable names to avoid runtime exceptions

--- web_crawler/mycraaawler.py
+++ web_crawler/mycraaawler.py
@@ -8,19 +8,12 @@ import argparse
 from BeautifulSoup import BeautifulSoup as bs
 
 
-def Enlances (nivel,asterisko,url):
+def obtainLink(nivel,asterisko,url):
     try:
         user_agent = " Mozilla /5.0 ( X11 ; U ; Linux x86_64 ; en -US ) AppleWebKit /534.7 ( KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
 
         _opener = urllib2.build_opener()
-        
-        urllib2.install_opener(url)
-
-
         _opener.addheaders = [('User - agent',user_agent ) ]
-        
-        
-        
         raw_code =_opener.open(url).read()
         soup = bs( raw_code )
         todos_enlances = [ link ('href') for link
@@ -34,11 +27,9 @@ def Enlances (nivel,asterisko,url):
     for link in todos_enlances : 
         #print link  
         if nivel < deep :   
-           Enlances(nivel+1,asterisko+'*',link)  
-           print "Ante gamhsou"
+                obtainLink(nivel+1,asterisko+'*',link)  
+                print "Hello World!"
           
-        
-
 parser = argparse.ArgumentParser(description = "Let's craawl the web guys!")
 
 parser.add_argument('url', nargs = 1, help = 'target url')
@@ -51,7 +42,7 @@ target_url = args.url.pop()
 
 deep = args.number_of_levels
 
-Enlances(1,"*",target_url) 
+obtainLink(1,"*",target_url) 
 
     
 
