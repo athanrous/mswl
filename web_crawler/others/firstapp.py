696488840de9cf2413c38ee32794d827ac05a917
Athanasios-Ilias <athanrous@gmail.com>
2012-01-30 19:40:56 +0200

    Create the Movie Class

--- web_crawler/others/firstapp.py
+++ web_crawler/others/firstapp.py
@@ -1,4 +1,29 @@
-#! /usr/bin/python
+#!/usr/bin/python
+
+class Movie:
+    def __init__(self,title,duration,price):
+        self.title = title
+        self.duration = duration
+        self.price = price
+    
+    def showTitle(self):
+        return self.title
+    
+    def showDuration(self):
+        return self.duration
+    
+    def showPrice(self):
+        return self.price
+        
+def main():
+    
+    # print ("This is my first application") #
+    monomaxos = Movie("Gladiator","240 mins","10 Euro")
+    
+    print (monomaxos.showPrice())
+    print (monomaxos.showDuration())
+    print  (monomaxos.showTitle())
+    
+if __name__ == "__main__": main()
 
 
-print ("This is my first application")
