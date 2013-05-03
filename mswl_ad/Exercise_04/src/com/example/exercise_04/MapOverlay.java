62a7565a6b50a695b512896714fa02c4b2246c3a
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2013-05-03 04:52:46 +0200

    Add the ListActivity class

--- mswl_ad/Exercise_04/src/com/example/exercise_04/MapOverlay.java
+++ mswl_ad/Exercise_04/src/com/example/exercise_04/MapOverlay.java
@@ -9,11 +9,12 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
 import com.google.android.maps.Projection;
 
-	public class MapOverlay extends Overlay {
+public class MapOverlay extends Overlay {
 
 	private Drawable mMarker;
 	private int mMarkerXOffset;
 	private int mMarkerYOffset;
+	private String text;
 
 	private GeoPoint mGeoPoint;
 
@@ -21,7 +22,13 @@ import com.google.android.maps.Projection;
 	{
 	mGeoPoint = geoPoint;	
 	}
-
+	
+	//We created this method in order to set text in a generic way
+	
+	public void setText(String text){ 
+		this.text = text;
+	}
+	
 	public void setDrawable(Drawable draw)
 	{
 	mMarker = draw;
@@ -29,27 +36,29 @@ import com.google.android.maps.Projection;
 
 	@Override
 	public void draw(Canvas canvas, MapView mapView, boolean shadow) {
-	         if (!shadow) {
+	         
+		if (!shadow) {
 	         
 	     
 	// Make sure to give mMarker bounds so it will draw in the overlay
-	final int intrinsicWidth = mMarker.getIntrinsicWidth();
-	final int intrinsicHeight = mMarker.getIntrinsicHeight();
-	mMarker.setBounds(0, 0, intrinsicWidth, intrinsicHeight);
+	        	 
+	        	 final int intrinsicWidth = mMarker.getIntrinsicWidth();
+	        	 final int intrinsicHeight = mMarker.getIntrinsicHeight();
+	        	 mMarker.setBounds(0, 0, intrinsicWidth, intrinsicHeight);
 
-	mMarkerXOffset = -(intrinsicWidth / 2);
-	mMarkerYOffset = -(intrinsicHeight / 2);
+	        	 mMarkerXOffset = -(intrinsicWidth / 2);
+	        	 mMarkerYOffset = -(intrinsicHeight / 2);
 
-	Paint paint = new Paint();
-	paint.setARGB(250,0,0,0);
-	paint.setAntiAlias(true);
-	paint.setFakeBoldText(true);
+	        	 Paint paint = new Paint();
+	        	 paint.setARGB(250,0,0,0);
+	        	 paint.setAntiAlias(true);
+	        	 paint.setFakeBoldText(true);
 
 	             Point point2 = new Point();
 	             Projection p = mapView.getProjection();
 	             p.toPixels(mGeoPoint, point2);
 	     
-	             canvas.drawText("Aquí estoy!", point2.x - intrinsicWidth , point2.y + intrinsicHeight, paint);
+	             canvas.drawText(text, point2.x - intrinsicWidth , point2.y + intrinsicHeight, paint);
 	             super.draw(canvas, mapView, shadow);
 	             drawAt(canvas, mMarker, point2.x + mMarkerXOffset, point2.y + mMarkerYOffset, shadow);
 
