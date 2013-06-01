93087bc788b773b51d76b26d18a2455316b3b691
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2013-06-01 13:50:29 +0200

    The main structure is done.Remaining the latitude and longitude assign

--- mswl_ad/Exercise_04/gen/com/example/exercise_04/R.java
+++ mswl_ad/Exercise_04/gen/com/example/exercise_04/R.java
@@ -22,23 +22,44 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int image_01=0x7f020001;
+        public static final int image_02=0x7f020002;
+        public static final int image_03=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080007;
+        public static final int description=0x7f080005;
+        public static final int image=0x7f080003;
+        public static final int mapView=0x7f080002;
+        public static final int pubs=0x7f080006;
         public static final int textList=0x7f080000;
+        public static final int textViewLocation=0x7f080001;
+        public static final int title=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_maps_ext=0x7f030001;
+        public static final int main_list=0x7f030002;
+        public static final int pub_item=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int maps_ext=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int headList=0x7f050004;
+        public static final int description1=0x7f050008;
+        public static final int description2=0x7f05000a;
+        public static final int description3=0x7f05000c;
+        public static final int headList=0x7f050003;
         public static final int hello_world=0x7f050002;
-        public static final int map_key=0x7f050003;
+        public static final int map_key=0x7f050007;
+        public static final int pub=0x7f050005;
+        public static final int title1=0x7f050006;
+        public static final int title2=0x7f050009;
+        public static final int title3=0x7f05000b;
+        public static final int title_activity_maps_ext=0x7f050004;
     }
     public static final class style {
         /** 
--- mswl_ad/Exercise_04/src/com/example/exercise_04/MainActivity.java
+++ mswl_ad/Exercise_04/src/com/example/exercise_04/MainActivity.java
@@ -1,20 +1,138 @@
 package com.example.exercise_04;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.exercise_04.MapNode;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
+	private MyAdapter mAdapter = null;
+	
+	private static ArrayList<MapNode> mapArray = new ArrayList<MapNode>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mAdapter = new MyAdapter(this);
+		setListAdapter(mAdapter);
+		
 	}
 
+	 protected void onListItemClick(ListView l, View v, int pos, long id) {
+
+	        boolean spam = ((pos + 1) % 3 == 0);
+
+	        if (spam) {
+
+	            Toast.makeText(this,
+	                    this.getResources().getString(R.string.pub),
+	                    Toast.LENGTH_SHORT).show();
+
+	        } else {
+
+	            pos = pos - ((pos + 1) / 3);
+
+	            MapNode selectedNode = mapArray.get(pos);
+	            Intent intentMapsExercise = new Intent(MainActivity.this,
+	                    MapsExtActivity.class);
+	            intentMapsExercise.putExtra("node", selectedNode);
+
+	            startActivity(intentMapsExercise);
+	        }
+	    }
+	
+	
+	
+	 
+
+	 
+	   public static class MyAdapter extends BaseAdapter {
+
+	        private Context mapContext;
+
+	        public MyAdapter(Context context) {
+	            // TODO Auto-generated constructor stub
+	           
+	            mapContext = context;
+	        }
+
+	        @Override
+	        public int getCount() {
+	            return mapArray.size() + (mapArray.size() / 2) + (mapArray.size() % 2);
+	        }
+
+	        @Override
+	        public Object getItem(int position) {
+	            return mapArray.get(position);
+	        }
+
+	        @Override
+	        public long getItemId(int position) {
+	            // TODO Auto-generated method stub
+	            
+	            return 0;
+	        }
+
+	        @Override
+	        public View getView(int position, View convertView, ViewGroup parent) {
+
+	            View view;
+
+	            LayoutInflater inflater = (LayoutInflater) mapContext
+	                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+	            boolean advertise = ((position + 1) % 3 == 0);
+
+	            if (advertise) {
+
+	                view = inflater.inflate(R.layout.pub_item, null);
+
+	                TextView titleTextView = (TextView) view
+	                        .findViewById(R.id.pubs);
+	                titleTextView.setText(R.string.pub);
+	                titleTextView.setBackgroundColor(Color.YELLOW);
+
+	            } else {
+
+	                position = position - ((position + 1) / 3);
+
+	                view = inflater.inflate(R.layout.main_list, null);
+	                TextView titleTextView = (TextView) view
+	                        .findViewById(R.id.title);
+	                titleTextView.setText(mapArray.get(position).mapTitle);
+
+	                TextView descriptionTextView = (TextView) view
+	                        .findViewById(R.id.description);
+	                descriptionTextView.setText(mapArray.get(position).mapDesc);
+
+	                ImageView imageViewLogo = (ImageView) view
+	                        .findViewById(R.id.image);
+	                imageViewLogo.setImageDrawable(mapContext.getResources()
+	                        .getDrawable(mapArray.get(position).mapImage));
+
+	            }
+	            return view;
+	        }
+
+	    }
+			
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
