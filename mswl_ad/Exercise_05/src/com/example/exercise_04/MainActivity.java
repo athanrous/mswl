bfa46fc122e092b792368bacc818b8daa0263c03
Athanasios-Ilias Rousinopoulos <athanrous@gmail.com>
2013-06-18 23:03:56 +0200

    Try to reset

---
+++ mswl_ad/Exercise_05/src/com/example/exercise_04/MainActivity.java
@@ -0,0 +1,192 @@
+package com.example.exercise_04;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.exercise_04.MapNode;
+
+public class MainActivity extends ListActivity {
+
+	private MyAdapter mAdapter = null;
+	
+	private static ArrayList<MapNode> mapArray = new ArrayList<MapNode>();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mAdapter = new MyAdapter(this);
+		setListAdapter(mAdapter);
+		setData();
+		
+	}
+
+	 protected void onListItemClick(ListView l, View v, int pos, long id) {
+
+	        boolean spam = ((pos + 1) % 3 == 0);
+
+	        if (spam) {
+
+	            Toast.makeText(this,
+	                    this.getResources().getString(R.string.pub),
+	                    Toast.LENGTH_SHORT).show();
+
+	        } else {
+
+	            pos = pos - ((pos + 1) / 3);
+
+	            MapNode selectedNode = mapArray.get(pos);
+	            Intent intentMapsExercise = new Intent(MainActivity.this,
+	                    MapsExtActivity.class);
+	            intentMapsExercise.putExtra("node", selectedNode);
+
+	            startActivity(intentMapsExercise);
+	        }
+	    }
+	
+	
+	
+	 private void setData() {
+
+	        mapArray.clear();
+
+	        MapNode myMnode = new MapNode();
+
+	        
+	        myMnode.mapTitle = this.getResources().getString(R.string.title1);
+	        myMnode.mapDesc = this.getResources().getString(
+	                R.string.description1);
+	        myMnode.mapImage = R.drawable.image_01;
+	        
+	        // Universidad Rey Juan Carlos
+	        myMnode.mapLon = -3.819945;
+	        myMnode.mapLat = 40.281523;
+	        
+		
+	        
+	        mapArray.add(myMnode);
+
+	        
+	        MapNode mynode2 = new MapNode();
+
+	        mynode2.mapTitle = this.getResources().getString(R.string.title2);
+	        mynode2.mapDesc = this.getResources().getString(
+	                R.string.description2);
+	        mynode2.mapImage = R.drawable.image_02;
+	        // University of Macedonia
+	        mynode2.mapLon = 22.959616;
+	        mynode2.mapLat = 40.624995;
+	        
+		
+	        
+	        mapArray.add(mynode2);
+
+	        MapNode mynode3 = new MapNode();
+
+	        mynode3.mapTitle = this.getResources().getString(R.string.title3);
+	        mynode3.mapDesc = this.getResources().getString(
+	                R.string.description3);
+	        mynode3.mapImage = R.drawable.image_03;
+	        // Democritus University of Thrace
+	        mynode3.mapLon = 24.887895;
+	        mynode3.mapLat = 41.141995;
+	        
+		
+	        
+	        mapArray.add(mynode3);
+
+	        mapArray.addAll(mapArray);
+
+	    } 
+
+	 
+	   public static class MyAdapter extends BaseAdapter {
+
+	        private Context mapContext;
+
+	        public MyAdapter(Context context) {
+	            // TODO Auto-generated constructor stub
+	           
+	            mapContext = context;
+	        }
+
+	        @Override
+	        public int getCount() {
+	            return mapArray.size() + (mapArray.size() / 2) + (mapArray.size() % 2);
+	        }
+
+	        @Override
+	        public Object getItem(int position) {
+	            return mapArray.get(position);
+	        }
+
+	        @Override
+	        public long getItemId(int position) {
+	            // TODO Auto-generated method stub
+	            
+	            return 0;
+	        }
+
+	        @Override
+	        public View getView(int position, View convertView, ViewGroup parent) {
+
+	            View view;
+
+	            LayoutInflater inflater = (LayoutInflater) mapContext
+	                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+	            boolean advertise = ((position + 1) % 3 == 0);
+
+	            if (advertise) {
+
+	                view = inflater.inflate(R.layout.pub_item, null);
+
+	                TextView titleTextView = (TextView) view
+	                        .findViewById(R.id.pubs);
+	                titleTextView.setText(R.string.pub);
+	                titleTextView.setBackgroundColor(Color.YELLOW);
+
+	            } else {
+
+	                position = position - ((position + 1) / 3);
+
+	                view = inflater.inflate(R.layout.main_list, null);
+	                TextView titleTextView = (TextView) view
+	                        .findViewById(R.id.title);
+	                titleTextView.setText(mapArray.get(position).mapTitle);
+
+	                TextView descriptionTextView = (TextView) view
+	                        .findViewById(R.id.description);
+	                descriptionTextView.setText(mapArray.get(position).mapDesc);
+
+	                ImageView imageViewLogo = (ImageView) view
+	                        .findViewById(R.id.image);
+	                imageViewLogo.setImageDrawable(mapContext.getResources()
+	                        .getDrawable(mapArray.get(position).mapImage));
+
+	            }
+	            return view;
+	        }
+
+	    }
+			
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
---
+++ mswl_ad/Exercise_05/src/com/example/exercise_04/MapsActivity.java
@@ -0,0 +1,122 @@
+package com.example.exercise_04;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+
+public class MapsActivity extends MapActivity {
+	
+	
+	private MapView mapView;
+    private MapController mapController;
+
+    private Location mLocation;
+    private LocationManager mLocationManager;
+    private MyLocationListener mLocationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_ext);
+
+        
+        mapView = (MapView) findViewById(R.id.mapView);
+
+        
+        mapView.setBuiltInZoomControls(true);
+        mapView.setClickable(true);
+        mapController = mapView.getController();
+        GeoPoint geoPoint = new GeoPoint((int) (0 * 1E6), (int) (0 * 1E6));
+        mapController.setZoom(18);
+        mapController.animateTo(geoPoint);
+
+        setLocationListener();
+
+    }
+
+    private void refreshMap() {
+
+        GeoPoint geoPoint = new GeoPoint((int) (mLocation.getLatitude() * 1E6),
+                (int) (mLocation.getLongitude() * 1E6));
+
+        mapController.setZoom(18);
+        mapController.animateTo(geoPoint);
+    }
+
+    private void setLocationListener() {
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        mLocationListener = new MyLocationListener();
+
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                5000, 15, mLocationListener);
+    }
+
+    public class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+
+            mLocation = location;
+
+            Log.d("MyLocationListener",
+                    "Location:\n * Latitude:\t"
+                            + String.valueOf(mLocation.getLatitude())
+                            + "\n* Longitude:\t"
+                            + String.valueOf(mLocation.getLongitude()));
+            refreshMap();
+        }
+        
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+    
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.maps_ext, menu);
+        return true;
+    }
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        mLocationManager.removeUpdates(mLocationListener);
+        super.onDestroy();
+    }
+
+}
